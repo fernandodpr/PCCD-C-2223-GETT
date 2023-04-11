09580087f442c775f447a3ff6eacd177e89f55da
Fernando de Peroy Rodríguez <contacto@fernandodpr.es>
2023-04-11 20:29:18 +0000

    Main listo

--- main.c
+++ main.c
@@ -17,7 +17,7 @@
 typedef enum {
     NO_INTERESADO, //0  El proceso no está interesado en acceder a la sección crítica actualmente
     SOLICITANTE, //1  El proceso está solicitando acceso a la sección crítica
-    ESPERANDO   //2  El proceso está esperando su turno para acceder a la sección crítica
+    FINALIZADO   //2  El proceso está esperando su turno para acceder a la sección crítica
 } Status;
 typedef enum {
     SOLICITUD, //0 
@@ -152,7 +152,7 @@ int main(int argc, char *argv[]) {
     
     //Aleatorizar la entrada en SC
 
-        bool   quiero_entrar;
+        bool   quiero_entrar=false;
         
         do {
             quiero_entrar = (double)rand() / RAND_MAX > PROBABILIDAD_ENTRADA;
@@ -191,19 +191,17 @@ int main(int argc, char *argv[]) {
         sleep(rand() % 5 + 4); // Dormir una cantidad de tiempo aleatoria entre 4 y 8 segundos    }
         printf("[Nodo %i]: He terminado la sección crítica",identificador_nodo);
 
-
-
-        //Salgo de la sección critica:
-            //Cambio mi estado a que no quiero entrar
-            quiero_entrar = false;
-            // Cambio mi estado a que ya no hay limitacion
-            estado=0;
-            //Notifico a los nodos que ya salí de SC
-            //De momento está en pseudocodigo
-            for(i=0;i<cantidaddenodos;i++){
-                //Enviar a cada nodo un mensaje de que ya salió de SC
-                printf("[Nodo %i]: Mensaje enviado a nodo %i. Contenido: Ya no quiero entrar en SC\n",identificador_nodo,nodovecino);
-            }
+        //Ahora notificamos la salida de la SC a los vecinos
+        for (int i=1;i<NODOSVECINOS; i++) {
+            Paquete peticion;
+            peticion.estado=FINALIZADO;
+            peticion.instruccion=ACK;
+            peticion.id_nodo=nodos[0];
+            peticion.num_ticket=ticketnum;
+            peticion.id_proceso=pid;//Para futuro para poder direccionar en multiples procesos
+            NetworkSend(nodos[i], &peticion); //TODO: Imprimir algun dato más desde esta función
+        }
+        quiero_entrar = false;
 
     }while (1); // Bucle para que funcione constantemente*/ 
     return 0;
