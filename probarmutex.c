8936d2db6916f9cd1931e5c04fe5a4ef3a085eb3
Alba Vazquez <runo-mola12@hotmail.es>
2023-05-04 18:56:01 +0200

    probarmutex

--- probarmutex.c
+++ probarmutex.c
@@ -51,33 +51,36 @@ sem_t sem_prioridades[5];
 struct Proceso* cola = NULL;   // Generar una lista aleatoria de 10 elementos
 struct Proceso* historial = NULL;   // Generar una lista aleatoria de 10 elementos
 
-
+void initparam();
 
 void * procesomutex(int * param){
     //pid_t hilo_pid = getpid();
 
     pid_t hilo_pid = gettid();
     
-    int contadorschilo=1;
-    int valorSemaforoSC;
-    int valorSemaforoAvisoNodos;
+    //int contadorschilo=1;
+    //int valorSemaforoSC;
+    //int valorSemaforoAvisoNodos;
     int nodos[1];
     nodos[0]=1;
+    printf("Acabo de entrar en procesomutex\n");
+    
     do{
 
         //Aleatorizar la entrada en SC
             bool aleatoriaentrada=true;
-                        printf("Entro en SC");
+            printf("Intentando entrar en la seccion critica...\n");
             do {
 
                 aleatoriaentrada = (double)rand() / RAND_MAX < PROBABILIDAD_ENTRADA;
                 if (!aleatoriaentrada) {
                     // Si no quiero entrar espero
-
+                    
                     sleep(rand()%5+1); //Dormir una cantidad de tiempo aleatoria entre 1 y 5 segundos
                 }else{}
             }while (!aleatoriaentrada); // 0 No interesado 1 SOLICITANTE
             printf("Entro en SC");
+            
             struct Proceso yomismo;
             yomismo.idProceso=hilo_pid;
             yomismo.atendido=0;
@@ -87,7 +90,7 @@ void * procesomutex(int * param){
             yomismo.prioridad=1;
             yomismo.ticket=rand() % 20; 
             
-            agregarProceso(cola, yomismo);
+            agregarProceso(&cola, yomismo);
             
             
             ordenarCola(&cola);
@@ -110,8 +113,8 @@ void * procesomutex(int * param){
             sleep(tiempoespera);
             cola->fin = time(NULL);       
 
-            agregarProceso(historial, *cola);
-            eliminarCabeza(cola);
+            agregarProceso(&historial, *cola);
+            eliminarCabeza(&cola);
 
             if(compararIdNodo(cola,nodos[0])){
                 //El sigueinte proceso está esperando en mi nodo
@@ -122,16 +125,18 @@ void * procesomutex(int * param){
            
             
             sleep(1);
-    }while(1);
+        }while(1);
+
+    return NULL;
     
 }
 
 int main(int argc, char *argv[]) {
-            printf("Hola");
+    printf("Hola\n");
     initparam();
     pthread_t pthtest[10];
     for (int i =0; i<2; i++) {
-        printf("Creo hilo");
+        printf("Creo hilo\n");
         pthread_create(&pthtest[i],NULL,(void *)procesomutex,NULL);   
     }
    
@@ -143,9 +148,6 @@ int main(int argc, char *argv[]) {
 }
 
 
-
-
-
 void initparam(){
     //INICIALIZACIÓN DE SEMAFOROS
     sem_init(&sem_SC,0,1);
@@ -155,8 +157,6 @@ void initparam(){
     sem_init(&sem_saleSC,0,0);
     sem_init(&sem_avisar_nodos_en_espera,0,0);
     sem_init(&sem_protec_var_estado,0,1);
-        sem_init(&sem_protec_lista,0,1);
-
-
+    sem_init(&sem_protec_lista,0,1);
 
 }
